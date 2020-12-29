d40fe96ee85e78b3305d101c4702671344154f87
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-29 20:43:35 -0300

    :wrench: chore: gulfile created

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require("gulp");
+var cleanCSS = require("gulp-clean-css");
+var autoprefixer = require("gulp-autoprefixer");
+var concat = require("gulp-concat");
+var htmlmin = require("gulp-htmlmin");
+
+gulp.task("styles", () => {
+  return gulp
+    .src("src/styles/main.css")
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("dist/styles"));
+});
+
+gulp.task("pages", () => {
+  return gulp
+    .src("src/views/index.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true,
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
